dad560a93bc2004a10882fb2949d11df0aaea3b6
Egor Kataev <lumennes@gmail.com>
2016-03-11 20:32:23 +0500

    first commit

---
+++ catalog/view/theme/default/template/module/myModul.tpl
@@ -0,0 +1,7 @@
+
+<div class="box">
+  <div class="box-heading"><?php echo $this->language->get('title'); ?></div>
+  <div class="box-content">
+		код модуля
+  </div>
+</div>
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opcarts.org/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opcarts.org/');
+
+// DIR
+define('DIR_APPLICATION', 'Z:/home/opcarts.org/www/catalog/');
+define('DIR_SYSTEM', 'Z:/home/opcarts.org/www/system/');
+define('DIR_LANGUAGE', 'Z:/home/opcarts.org/www/catalog/language/');
+define('DIR_TEMPLATE', 'Z:/home/opcarts.org/www/catalog/view/theme/');
+define('DIR_CONFIG', 'Z:/home/opcarts.org/www/system/config/');
+define('DIR_IMAGE', 'Z:/home/opcarts.org/www/image/');
+define('DIR_CACHE', 'Z:/home/opcarts.org/www/system/storage/cache/');
+define('DIR_DOWNLOAD', 'Z:/home/opcarts.org/www/system/storage/download/');
+define('DIR_LOGS', 'Z:/home/opcarts.org/www/system/storage/logs/');
+define('DIR_MODIFICATION', 'Z:/home/opcarts.org/www/system/storage/modification/');
+define('DIR_UPLOAD', 'Z:/home/opcarts.org/www/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'opcarts');
+define('DB_PASSWORD', '123');
+define('DB_DATABASE', 'opcarts');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
